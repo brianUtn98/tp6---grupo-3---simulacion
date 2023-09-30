4447b3cfa4fb91cdd1887c4e8a52ac78f423a21f
Brian Monroy <brian.gmonroy98@gmail.com>
2023-09-30 02:17:59 -0300

    mejorar resultados y agregar reportes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import fs from "fs";
+
 const H_V = Infinity;
 
 function msleep(n: number) {
@@ -6,6 +8,36 @@ function msleep(n: number) {
 function sleep(n: number) {
   msleep(n * 1000);
 }
+
+const formatNumber = (n: number) => {
+  if (isNaN(n)) return 0;
+  return n.toFixed(2);
+};
+
+const formatTimeWitDays = (seconds: number): string => {
+  const days = Math.floor(seconds / 86400);
+  const hs = Math.floor((seconds % 86400) / 3600);
+  const mns = Math.floor((seconds % 3600) / 60);
+  const sns = Math.floor((seconds % 3600) % 60);
+
+  const hours = hs >= 10 ? hs : `0${hs}`;
+  const minutes = mns >= 10 ? mns : `0${mns}`;
+  const seconds_ = sns >= 10 ? sns : `0${sns}`;
+
+  return `Day ${days} - ${hours}:${minutes}:${seconds_}`;
+};
+
+const formatTime = (seconds: number): string => {
+  const hs = Math.floor(seconds / 3600);
+  const mns = Math.floor((seconds % 3600) / 60);
+  const sns = Math.floor((seconds % 3600) % 60);
+
+  const hours = hs >= 10 ? hs : `0${hs}`;
+  const minutes = mns >= 10 ? mns : `0${mns}`;
+  const seconds_ = sns >= 10 ? sns : `0${sns}`;
+
+  return `${hours}:${minutes}:${seconds_}`;
+};
 
 const Simulacion = (NA: number, NB: number, NC: number, TF: number) => {
   let T = 0;
@@ -27,15 +59,9 @@ const Simulacion = (NA: number, NB: number, NC: number, TF: number) => {
   let ITOB: number[] = new Array(NB).fill(0);
   let STOC: number[] = new Array(NC).fill(-1);
   let ITOC: number[] = new Array(NC).fill(0);
-  let STAA = 0;
-  let STAB = 0;
-  let STAC = 0;
-  let STSA = 0;
-  let STSB = 0;
-  let STSC = 0;
-  let STLLA = 0;
-  let STLLB = 0;
-  let STLLC = 0;
+  let STA = 0;
+  let STS = 0;
+  let STLL = 0;
   let NTCB = 0;
   let NTESP = 0;
   let NumAtencionesA = new Array(NA).fill(0);
@@ -43,38 +69,175 @@ const Simulacion = (NA: number, NB: number, NC: number, TF: number) => {
   let NumAtencionesC = new Array(NC).fill(0);
 
   const CalcularYMostrarResultados = (): void => {
+    console.log("----------------------------------------");
+    console.log("Resultados");
+
     const PTOA = STOA.map((v) => (v * 100) / T);
     const PTOB = STOB.map((v) => (v * 100) / T);
     const PTOC = STOC.map((v) => (v * 100) / T);
 
-    console.log("PTOA: ", PTOA);
-    console.log("PTOB: ", PTOB);
-    console.log("PTOC: ", PTOC);
+    console.log(
+      "Porcentaje de tiempo ocioso puestos A: ",
+      PTOA.map((v) => formatNumber(v) + "%")
+    );
+    console.log(
+      "Porcentaje de tiempo ocioso puestos B: ",
+      PTOB.map((v) => formatNumber(v) + "%")
+    );
+    console.log(
+      "Porcentaje de tiempo ocioso puestos C: ",
+      PTOC.map((v) => formatNumber(v) + "%")
+    );
 
-    const PESA = (STSA - STLLA - STAA) / NTA;
-    const PEEV = (STSB - STLLB - STAB) / NTB;
-    const PEPR = (STSC - STLLC - STAC) / NTC;
-    console.log(`(${STSC} - ${STLLC} - ${STAC}) / ${NTC}`);
+    const PES = (STS - STLL - STA) / (NTA + NTB + NTC);
 
-    console.log("PESA: ", PESA);
-    console.log("PEEV: ", PEEV);
-    console.log("PEPR: ", PEPR);
+    console.log("Promedio de espera en el Sistema: ", formatTime(PES));
 
-    const PPSA = (STSA - STLLA) / NTA;
-    const PPSB = (STSB - STLLB) / NTB;
-    const PPSC = (STSC - STLLC) / NTC;
+    const PPS = (STS - STLL) / (NTA + NTB + NTC);
 
-    console.log("PPSA: ", PPSA);
-    console.log("PPSB: ", PPSB);
-    console.log("PPSC: ", PPSC);
+    console.log("Promedio de permanencia en el Sistema: ", formatTime(PPS));
 
-    const PCD = (NTCB * 100) / (NTC + NTCB);
-    console.log("PCD: ", PCD);
+    const PCB = (NTCB * 100) / (NTC + NTCB);
+    console.log(
+      "Porcentaje de Operaciones C derivadas a B: ",
+      formatNumber(PCB) + "%"
+    );
 
     const PEO = (NTESP * 100) / (NTA + NTB + NTC);
 
-    console.log("PEO: ", PEO);
+    console.log(
+      "Porcentaje de Operaciones que devieron esperar: ",
+      formatNumber(PEO) + "%"
+    );
+
+    console.log("Servidores A: ", NA);
+    console.log("Servidores B: ", NB);
+    console.log("Servidores C: ", NC);
+
+    const CostoXHoraA = 0.4536;
+
+    const CostoXHoraB = 0.752;
+
+    const CostoXHoraC = 0.172;
+
+    const CostoDiarioA = CostoXHoraA * 24 * NA;
+
+    const CostoDiarioB = CostoXHoraB * 24 * NB;
+
+    const CostoDiarioC = CostoXHoraC * 24 * NC;
+
+    console.log("Costo Diario A: ", formatNumber(CostoDiarioA) + "USD");
+
+    console.log("Costo Diario B: ", formatNumber(CostoDiarioB) + "USD");
+
+    console.log("Costo Diario C: ", formatNumber(CostoDiarioC) + "USD");
+
+    const CostoEficienteA =
+      CostoDiarioA *
+      (1 - PTOA.map((v) => v / 100).reduce((a, b) => a + b) / NA);
+
+    const CostoPerdidoA = CostoDiarioA - CostoEficienteA;
+
+    const CostoEficienteB =
+      CostoDiarioB *
+      (1 - PTOB.map((v) => v / 100).reduce((a, b) => a + b) / NB);
+
+    const CostoPerdidoB = CostoDiarioB - CostoEficienteB;
+
+    const CostoEficienteC =
+      CostoDiarioC *
+        (1 - PTOC?.map((v) => v / 100)?.reduce((a, b) => a + b, 0) / NC) ?? 0;
+
+    const CostoPerdidoC = CostoDiarioC - CostoEficienteC ?? 0;
+
+    console.log("Costo Eficiente A: ", formatNumber(CostoEficienteA) + "USD");
+
+    console.log("Costo Perdido A: ", formatNumber(CostoPerdidoA) + "USD");
+
+    console.log("Costo Eficiente B: ", formatNumber(CostoEficienteB) + "USD");
+
+    console.log("Costo Perdido B: ", formatNumber(CostoPerdidoB) + "USD");
+
+    console.log("Costo Eficiente C: ", formatNumber(CostoEficienteC) + "USD");
+
+    console.log("Costo Perdido C: ", formatNumber(CostoPerdidoC) + "USD");
+
+    const CostoTotalDiario = CostoDiarioA + CostoDiarioB + CostoDiarioC;
+
+    console.log("Costo Total Diario: ", formatNumber(CostoTotalDiario) + "USD");
+
+    console.log("Tiempo total de simulacion: ", formatTimeWitDays(T));
+
+    console.log("----------------------------------------");
+    const reportFile = `./report-NA${NA}-NB${NB}-NC${NC}-${Date.now()}.txt`;
+
+    fs.writeFileSync(
+      reportFile,
+      `Reporte de simulacion ${new Date().toISOString()}\n`
+    );
+
+    fs.appendFileSync(reportFile, "Porcentaje de tiempo ocioso puestos A: \n");
+    PTOA.forEach((v, i) => {
+      fs.appendFileSync(reportFile, `Puesto ${i + 1}: ${v.toFixed(2)}%\n`);
+    });
+    fs.appendFileSync(reportFile, "----------------------------------------\n");
+    fs.appendFileSync(reportFile, "Porcentaje de tiempo ocioso puestos B: \n");
+    PTOB.forEach((v, i) => {
+      fs.appendFileSync(reportFile, `Puesto ${i + 1}: ${v.toFixed(2)}%\n`);
+    });
+    fs.appendFileSync(reportFile, "----------------------------------------\n");
+    fs.appendFileSync(reportFile, "Porcentaje de tiempo ocioso puestos C: \n");
+    PTOC.forEach((v, i) => {
+      fs.appendFileSync(reportFile, `Puesto ${i + 1}: ${v.toFixed(2)}%\n`);
+    });
+    fs.appendFileSync(reportFile, "----------------------------------------\n");
+    fs.appendFileSync(
+      reportFile,
+      `Promedio de espera en el Sistema: ${formatTime(PES)}\n`
+    );
+    fs.appendFileSync(
+      reportFile,
+      `Promedio de permanencia en el Sistema: ${formatTime(PPS)}\n`
+    );
+    fs.appendFileSync(
+      reportFile,
+      `Porcentaje de Operaciones C derivadas a B: ${PCB.toFixed(2)}%\n`
+    );
+    fs.appendFileSync(
+      reportFile,
+      `Porcentaje de Operaciones que devieron esperar: ${PEO.toFixed(2)}%\n`
+    );
+    fs.appendFileSync(reportFile, "----------------------------------------\n");
+    fs.appendFileSync(reportFile, `Servidores A: ${NA}\n`);
+    fs.appendFileSync(reportFile, `Servidores B: ${NB}\n`);
+    fs.appendFileSync(reportFile, `Servidores C: ${NC}\n`);
+    fs.appendFileSync(reportFile, "----------------------------------------\n");
+    fs.appendFileSync(reportFile, "----------Costos----------\n");
+    fs.appendFileSync(
+      reportFile,
+      `Costo Diario A: ${CostoDiarioA.toFixed(2)}USD\n`
+    );
+    fs.appendFileSync(
+      reportFile,
+      `Costo Diario B: ${CostoDiarioB.toFixed(2)}USD\n`
+    );
+    fs.appendFileSync(
+      reportFile,
+      `Costo Diario C: ${CostoDiarioC.toFixed(2)}USD\n`
+    );
+    fs.appendFileSync(
+      reportFile,
+      `Costo Total Diario: ${CostoTotalDiario.toFixed(2)}USD\n`
+    );
+    fs.appendFileSync(reportFile, "----------------------------------------\n");
+    fs.appendFileSync(
+      reportFile,
+      `Tiempo total de simulacion: ${formatTimeWitDays(T)}\n`
+    );
+
+    console.log("Guardando resultados en ", reportFile);
   };
+  formatTime;
 
   const IntervaloOperaciones = (): number => {
     let R = Math.random();
@@ -155,8 +318,6 @@ const Simulacion = (NA: number, NB: number, NC: number, TF: number) => {
   const DistribuirProceso = (): string => {
     const R = Math.random();
 
-    console.log("R: ", R);
-
     if (R <= 0.1449) {
       return "C";
     } else if (R <= 0.2096) {
@@ -168,7 +329,7 @@ const Simulacion = (NA: number, NB: number, NC: number, TF: number) => {
 
   const EncolarA = (): void => {
     NSA++;
-    STLLA += T;
+    STLL += T;
 
     if (NSA <= NA) {
       let i = BuscarLibre(TPSA);
@@ -179,7 +340,7 @@ const Simulacion = (NA: number, NB: number, NC: number, TF: number) => {
 
       TPSA[i] = T + TSA;
 
-      STAA += TSA;
+      STA += TSA;
     } else {
       NTESP++;
     }
@@ -189,7 +350,7 @@ const Simulacion = (NA: number, NB: number, NC: number, TF: number) => {
 
   const EncolarB = (): void => {
     NSB++;
-    STLLB += T;
+    STLL += T;
 
     if (NSB <= NB) {
       let i = BuscarLibre(TPSB);
@@ -200,7 +361,7 @@ const Simulacion = (NA: number, NB: number, NC: number, TF: number) => {
 
       TPSB[i] = T + TEV;
 
-      STAB += TEV;
+      STA += TEV;
     } else {
       NTESP++;
     }
@@ -220,38 +381,31 @@ const Simulacion = (NA: number, NB: number, NC: number, TF: number) => {
 
       TPSC[i] = T + TPR;
 
-      STAC += TPR;
+      STA += TPR;
 
       NTC++;
 
-      STLLC += T;
+      STLL += T;
     } else if (NSB < NB) {
-      console.log("Espera - Llegada");
-      // sleep(2);
       NSC--;
       NTCB++;
       EncolarB();
     } else {
       NTC++;
 
-      STLLC += T;
+      STLL += T;
 
       NTESP++;
     }
   };
 
   const Llegada = (): void => {
-    console.log("Llegada");
     T = TPLL;
     const IO = IntervaloOperaciones();
 
     TPLL = T + IO;
 
-    console.log("TPLL: ", TPLL);
-
     const cola = DistribuirProceso();
-
-    console.log("Cola: ", cola);
 
     if (cola === "A") {
       EncolarA();
@@ -269,8 +423,6 @@ const Simulacion = (NA: number, NB: number, NC: number, TF: number) => {
   const SalidaA = (i: number): void => {
     NumAtencionesA[i]++;
 
-    console.log("Salida A");
-
     T = TPSA[i];
 
     NSA--;
@@ -280,18 +432,16 @@ const Simulacion = (NA: number, NB: number, NC: number, TF: number) => {
 
       TPSA[i] = T + TSA;
 
-      STAA += TSA;
+      STA += TSA;
     } else {
       ITOA[i] = T;
       TPSA[i] = H_V;
     }
 
-    STSA += T;
+    STS += T;
   };
 
   const SalidaB = (j: number): void => {
-    console.log("Salida B");
-
     NumAtencionesB[j]++;
 
     T = TPSB[j];
@@ -303,18 +453,15 @@ const Simulacion = (NA: number, NB: number, NC: number, TF: number) => {
 
       TPSB[j] = T + TEV;
 
-      STAB += TEV;
+      STA += TEV;
     } else if (NSC > NC) {
-      console.log("Espera - Salida");
-      // sleep(2);
-
       NSC--;
 
       NSB++;
 
       const TEV = TiempoExportarVideos();
 
-      STAB += TEV;
+      STA += TEV;
 
       TPSB[j] = T + TEV;
 
@@ -324,12 +471,10 @@ const Simulacion = (NA: number, NB: number, NC: number, TF: number) => {
       TPSB[j] = H_V;
     }
 
-    STSB += T;
+    STS += T;
   };
 
   const SalidaC = (k: number): void => {
-    console.log("Salida C");
-
     NumAtencionesC[k]++;
 
     T = TPSC[k];
@@ -341,13 +486,13 @@ const Simulacion = (NA: number, NB: number, NC: number, TF: number) => {
 
       TPSC[k] = T + TPR;
 
-      STAC += TPR;
+      STA += TPR;
     } else {
       ITOC[k] = T;
       TPSC[k] = H_V;
     }
 
-    STSC += T;
+    STS += T;
   };
 
   const MenorTPS = (): { cola: string; TPS: number; puesto: number } => {
@@ -380,11 +525,9 @@ const Simulacion = (NA: number, NB: number, NC: number, TF: number) => {
     let iteraciones = 0;
 
     do {
-      console.log("T: ", T);
+      console.log("T: ", formatTimeWitDays(T));
 
       const { cola, TPS, puesto } = MenorTPS();
-      console.log("TPLL: ", TPLL);
-      console.log("TPS: ", TPS);
 
       if (TPLL <= TPS) {
         Llegada();
@@ -403,31 +546,21 @@ const Simulacion = (NA: number, NB: number, NC: number, TF: number) => {
       }
     } while (T < TF || NSA > 0 || NSB > 0 || NSC > 0);
 
-    // console.log("Iteraciones: ", iteraciones);
-    // console.log("STLLA: ", STLLA);
-    // console.log("STLLB: ", STLLB);
-    // console.log("STLLC: ", STLLC);
-    // console.log("STAA: ", STAA);
-    // console.log("STAB: ", STAB);
-    // console.log("STAC: ", STAC);
-    // console.log("STSA: ", STSA);
-    // console.log("STSB: ", STSB);
-    // console.log("STSC: ", STSC);
-    // console.log("NTA: ", NTA);
-    // console.log("NTB: ", NTB);
-    // console.log("NTC: ", NTC);
-    // console.log("NTCB: ", NTCB);
-    // console.log(STOA);
-    // console.log(STOB);
-    // console.log(STOC);
-    // console.log("Atenciones A: ", NumAtencionesA);
-    // console.log("Atenciones B: ", NumAtencionesB);
-    // console.log("Atenciones C: ", NumAtencionesC);
-
     CalcularYMostrarResultados();
   };
 
   return Empezar;
 };
 
-Simulacion(1, 1, 1, 31536000)();
+const TF = 31536000;
+
+Simulacion(1, 1, 1, TF)();
+Simulacion(1, 1, 0, TF)();
+Simulacion(2, 2, 0, TF)();
+Simulacion(2, 2, 1, TF)();
+Simulacion(2, 3, 1, TF)();
+Simulacion(2, 3, 0, TF)();
+Simulacion(3, 2, 1, TF)();
+Simulacion(3, 2, 0, TF)();
+Simulacion(3, 3, 1, TF)();
+Simulacion(3, 3, 0, TF)();
